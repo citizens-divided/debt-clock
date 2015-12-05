525e585269d95c7b810a77567bb1ee920634e005
bitmage <q@bitmage.net>
2015-12-05 16:29:11 -0700

    added libs, data

--- app.coffee
+++ app.coffee
@@ -1,8 +1,40 @@
 data =
-  earnings: [
-      year: 2008
-    ,
-  ]
+
+  exampleEarnings:
+    "Hillary Clinton":
+      income: 300000
+      twitter: "@hillaryclinton"
+
+  assumptions:
+    minTaxableAge: 16
+
+  me:
+    age: 24
+    earnings: [
+        year: 2008
+        income: 40000
+      ,
+        year: 2009
+        income: 45000
+      ,
+        year: 2010
+        income: 45000
+      ,
+        year: 2011
+        income: 50000
+      ,
+        year: 2012
+        income: 55000
+      ,
+        year: 2013
+        income: 60000
+      ,
+        year: 2014
+        income: 65000
+      ,
+        year: 2015
+        income: 70000
+    ]
 
 wrapDiv = (text) ->
   "<div>#{text}</div>"
--- app.js
+++ app.js
@@ -3,11 +3,45 @@
   var data, wrapDiv;
 
   data = {
-    earnings: [
-      {
-        year: 2008
+    exampleEarnings: {
+      "Hillary Clinton": {
+        income: 300000,
+        twitter: "@hillaryclinton"
       }
-    ]
+    },
+    assumptions: {
+      minTaxableAge: 16
+    },
+    me: {
+      age: 24,
+      earnings: [
+        {
+          year: 2008,
+          income: 40000
+        }, {
+          year: 2009,
+          income: 45000
+        }, {
+          year: 2010,
+          income: 45000
+        }, {
+          year: 2011,
+          income: 50000
+        }, {
+          year: 2012,
+          income: 55000
+        }, {
+          year: 2013,
+          income: 60000
+        }, {
+          year: 2014,
+          income: 65000
+        }, {
+          year: 2015,
+          income: 70000
+        }
+      ]
+    }
   };
 
   wrapDiv = function(text) {
